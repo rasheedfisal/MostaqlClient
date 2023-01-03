9fefa2ff1073926373c3f15055d2fba093bc3c1c
DELL <rasheed.fisal2015@gmail.com>
2023-01-03 06:54:12 +0200

    removed1

--- app/api/axios.ts
+++ app/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { ILoginResponse } from "../../typings";
 
-const BASE_URL = "http://localhost:3000/api/v1/";
+const BASE_URL = "https://calm-cyan-bullfrog-tie.cyclic.app/";
 
 export const authApi = axios.create({
   baseURL: BASE_URL,
