1051c547504d2e387100acee0efc45f77b83e0c8
DELL <rasheed.fisal2015@gmail.com>
2023-01-04 11:21:40 +0200

    change url

--- app/api/axios.ts
+++ app/api/axios.ts
@@ -1,9 +1,8 @@
 import axios from "axios";
-import Cookies from "js-cookie";
 import { ILoginResponse } from "../../typings";
 
-const BASE_URL = "http://localhost:3000/api/v1/";
-// const BASE_URL = "https://calm-cyan-bullfrog-tie.cyclic.app/api/v1/";
+// const BASE_URL = "http://localhost:3000/api/v1/";
+const BASE_URL = "https://calm-cyan-bullfrog-tie.cyclic.app/api/v1/";
 
 export const authApi = axios.create({
   baseURL: BASE_URL,
