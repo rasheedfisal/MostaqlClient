e0f621c076d736fc97865ca1dddee2fb7311027e
DELL <rasheed.fisal2015@gmail.com>
2023-01-20 19:49:12 +0200

    fix u190

--- app/(dashboard)/chat/page.tsx
+++ app/(dashboard)/chat/page.tsx
@@ -368,7 +368,7 @@ const page = () => {
             <FormProvider {...methods}>
               <form noValidate onSubmit={methods.handleSubmit(onSubmitHandler)}>
                 <div className="flex items-center justify-between w-full p-3 border-t border-gray-300">
-                  <FormSearch label="Search" name="message" type="search" />
+                  <FormSearch label="message..." name="message" type="text" />
                   <button type="submit">
                     <svg
                       className="w-5 h-5 text-gray-500 origin-center transform rotate-90"
