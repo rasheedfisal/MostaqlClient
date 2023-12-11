fb48b2201aa839cb549799018884699f96830cc7
DELL <rasheed.fisal2015@gmail.com>
2023-12-12 01:23:51 +0200

    new003

--- app/api/axios.ts
+++ app/api/axios.ts
@@ -3,17 +3,17 @@ import { ILoginResponse } from "../../typings";
 
 // const BASE_URL = "http://localhost:3000/api/v1/";
 // const BASE_URL = "https://calm-cyan-bullfrog-tie.cyclic.app/api/v1/";
-// const BASE_URL = "http://194.195.87.30:91/api/v1/";
+const BASE_URL = "http://194.195.87.30:91/api/v1/";
 
 export const authApi = axios.create({
-  baseURL: process.env.BASE_URL,
+  baseURL: BASE_URL,
   withCredentials: true,
 });
 
 authApi.defaults.headers.common["Content-Type"] = "application/json";
 
 export const privateAuthApi = axios.create({
-  baseURL: process.env.BASE_URL,
+  baseURL: BASE_URL,
   withCredentials: true,
 });
 
